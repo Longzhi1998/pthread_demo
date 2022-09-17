29b3838fdb15c2de6ebbda7e94f53ab301a61d02
lz1998 <1693254825@qq.com>
2022-09-17 19:15:13 +0800

    添加了pthread_join，实现主线程阻塞，而非sleep

--- main.cpp
+++ main.cpp
@@ -1,5 +1,39 @@
+#include <math.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int mian(int argc, char **argv) {
-return 0; 
+void* th_fn(void *arg) {
+  int distance = *(int*)arg;
+  for (int i = 0; i <= distance; i++) {
+    printf("%lx run %d\n", pthread_self(), i);
+    int time = (int)(drand48() * 100000);
+    usleep(time);
+  }
+  return (void*)0;
+}
+
+int main() {
+  int err;
+  int distance = 50;
+  pthread_t rabbit, turble;  //定义线程标识符
+
+  // 创建rabbit线程
+  err = pthread_create(&rabbit, NULL, th_fn, (void*)&distance);
+  if (err != 0) {
+    printf("pthread_create error\n");
+    return -1;
+  }
+
+  err = pthread_create(&turble, NULL, th_fn, (void*)&distance);
+  if (err != 0) {
+    printf("pthread_create error\n");
+    return -1;
+  }
+  pthread_join(rabbit, NULL);
+  pthread_join(turble, NULL);
+  printf("main thread id %lx\n", pthread_self());
+  printf("Finish!\n");
+  return 0;
 }
